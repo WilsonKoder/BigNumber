f526334704cb00b3d0aa453bf87fd63703428f53
Wilson Koder <wilson.koder.111@gmail.com>
2015-05-17 16:07:27 +0800

    add main

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,12 @@
 /**
  * Created by Wilson on 5/17/2015.
  */
-public class Program {
-    public static void main() {
 
+public class Program {
+    public static void main(String[] args) {
+        BigNumber num = new BigNumber("65536");
+        for (;;) {
+            System.out.println(num.doubleNumber());
+        }
     }
 }
